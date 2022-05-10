d0b295949c609c84fbdf186e570fe587a2d774bd
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-05-09 19:55:54 -0700

    Challenge corrected, didn't define as long

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         byte challengeByte = 42;
         short challengeShort = 12345;
         int challengeInt = 12345678;
-        int challengeLong = 50000 + (10 * (challengeInt + challengeShort + challengeByte));
+        long challengeLong = 50000L + 10L * (challengeInt + challengeShort + challengeByte);
+        System.out.println(challengeLong);
     }
 }
